a3d642410246c7a68009c3e8d40cbde453a2a763
Julio Cesar <caesarconsultoria@gmail.com>
2024-07-16 13:38:42 -0300

    Projeto criado

---
+++ src/main/java/com/caesar/caesarprivacy/CaesarprivacyApplication.java
@@ -0,0 +1,13 @@
+package com.caesar.caesarprivacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaesarprivacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaesarprivacyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caesar/caesarprivacy/CaesarprivacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caesar.caesarprivacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaesarprivacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
